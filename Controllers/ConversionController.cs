0580176e3facfbb6a9f24086c3eb28d8564a8f39
yogeshktiwri23 <50910616+yogeshktiwri23@users.noreply.github.com>
2023-09-26 21:38:50 +0530

    Add files via upload

---
+++ Controllers/ConversionController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+
+    [Route("api/conversion")]
+    [ApiController]
+    public class ConversionController : ControllerBase
+    {
+        private readonly IConversionService _conversionService;
+
+        public ConversionController(IConversionService conversionService)
+        {
+            _conversionService = conversionService;
+        }
+
+        [HttpGet("convert")]
+        public IActionResult ConvertUnits(double value, string fromUnit, string toUnit, string conversionType)
+        {
+            try
+            {
+                double result = _conversionService.ConvertUnits(value, fromUnit, toUnit, conversionType);
+                return Ok(result);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+
+}
+
